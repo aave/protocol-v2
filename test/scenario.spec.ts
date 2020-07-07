5b840dd577e259b7231e66437f04650059692e6d
The3D <emilio@aave.com>
2020-07-07 14:04:38 +0200

    Fixed interest redirection tests

--- test/scenario.spec.ts
+++ test/scenario.spec.ts
@@ -12,7 +12,7 @@ BigNumber.config({DECIMAL_PLACES: 0, ROUNDING_MODE: BigNumber.ROUND_DOWN});
 
 const scenarioFolder = './test/helpers/scenarios/';
 
-const selectedScenarios: string[] = ['rebalance-stable-rate.json'];
+const selectedScenarios: string[] = [];
 
 fs.readdirSync(scenarioFolder).forEach((file) => {
   if (selectedScenarios.length > 0 && !selectedScenarios.includes(file)) return;
