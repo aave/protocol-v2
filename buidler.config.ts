4b8962d38f6eba6450fd48144940f2e32acf637a
The3D <emilio@aave.com>
2020-09-14 11:48:52 +0200

    Commented buidler gas reporter

--- buidler.config.ts
+++ buidler.config.ts
@@ -8,7 +8,7 @@ usePlugin('buidler-typechain');
 usePlugin('solidity-coverage');
 usePlugin('@nomiclabs/buidler-waffle');
 usePlugin('@nomiclabs/buidler-etherscan');
-usePlugin('buidler-gas-reporter');
+//usePlugin('buidler-gas-reporter');
 
 const DEFAULT_BLOCK_GAS_LIMIT = 10000000;
 const DEFAULT_GAS_PRICE = 10;
