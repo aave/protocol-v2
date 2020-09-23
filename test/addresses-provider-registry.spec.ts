7c892ec4ba658fbb9a5389efc6147e97a65f3125
The3D <emilio@aave.com>
2020-09-23 12:34:53 +0200

    added test on removal of an addresses provider

--- test/addresses-provider-registry.spec.ts
+++ test/addresses-provider-registry.spec.ts
@@ -1,5 +1,5 @@
 import {TestEnv, makeSuite} from './helpers/make-suite';
-import {RAY, APPROVAL_AMOUNT_LENDING_POOL} from '../helpers/constants';
+import {RAY, APPROVAL_AMOUNT_LENDING_POOL, ZERO_ADDRESS} from '../helpers/constants';
 import {convertToCurrencyDecimals} from '../helpers/contracts-helpers';
 import {ProtocolErrors} from '../helpers/types';
 
@@ -14,20 +14,10 @@ makeSuite('AddressesProviderRegistry', (testEnv: TestEnv) => {
     const providers = await registry.getAddressesProvidersList();
 
     expect(providers.length).to.be.equal(1, "Invalid length of the addresses providers list");
-    expect(providers[1].toString()).to.be.equal(addressesProvider.address, " Invalid addresses provider added to the list");
+    expect(providers[0].toString()).to.be.equal(addressesProvider.address, " Invalid addresses provider added to the list");
 
   });
 
-  it('Checks the addresses provider is added to the registry', async () => {
-    
-    const {addressesProvider, registry} = testEnv;
-
-    const providers = await registry.getAddressesProvidersList();
-
-    expect(providers.length).to.be.equal(1, "Invalid length of the addresses providers list");
-    expect(providers[1].toString()).to.be.equal(addressesProvider.address, " Invalid addresses provider added to the list");
-
-  });
 
   it('Registers a new mock addresses provider', async () => {
     
@@ -39,24 +29,27 @@ makeSuite('AddressesProviderRegistry', (testEnv: TestEnv) => {
     const providers = await registry.getAddressesProvidersList();
 
     expect(providers.length).to.be.equal(2, "Invalid length of the addresses providers list");
-    expect(providers[2].toString()).to.be.equal(users[1].address, " Invalid addresses provider added to the list");
+    expect(providers[1].toString()).to.be.equal(users[1].address, " Invalid addresses provider added to the list");
 
   });
 
-  it('Registers a new mock addresses provider', async () => {
+  it('Removes the mock addresses provider', async () => {
     
-    const {users, registry} = testEnv;
+    const {users, registry, addressesProvider} = testEnv;
 
-    //simulating an addresses provider using the users[1] wallet address
-    await registry.registerAddressesProvider(users[1].address, "2");
+    //checking the isAddressesProviderRegistered function
+    const id = await registry.isAddressesProviderRegistered(users[1].address);
+    
+    expect(id).to.be.equal("2", "Invalid isRegistered return value");
+    
+    await registry.unregisterAddressesProvider(users[1].address);
 
     const providers = await registry.getAddressesProvidersList();
 
     expect(providers.length).to.be.equal(2, "Invalid length of the addresses providers list");
-    expect(providers[2].toString()).to.be.equal(users[1].address, " Invalid addresses provider added to the list");
+    expect(providers[0].toString()).to.be.equal(addressesProvider.address, " Invalid addresses provider added to the list");
+    expect(providers[1].toString()).to.be.equal(ZERO_ADDRESS, " Invalid addresses");
 
   });
-
-
 });
 
