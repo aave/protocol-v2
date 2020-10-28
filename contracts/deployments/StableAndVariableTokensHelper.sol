1ef1f614bd16632f8846721256700bd32f2f93cf
David Racero <canillas.mail@gmail.com>
2020-10-28 18:13:31 +0100

    Remove extra whitespace

--- contracts/deployments/StableAndVariableTokensHelper.sol
+++ contracts/deployments/StableAndVariableTokensHelper.sol
@@ -18,7 +18,7 @@ contract StableAndVariableTokensHelper is Ownable {
   }
 
   function concat(string memory a, string memory b) internal pure returns (string memory) {
-    return string(abi.encodePacked(a, ' ', b));
+    return string(abi.encodePacked(a, b));
   }
 
   function initDeployment(
