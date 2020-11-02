6d009f796d4758a9df03638ad7892f562adec738
David Racero <canillas.mail@gmail.com>
2020-11-02 15:47:02 +0100

    Moved WETH9.sol to mock/dependencies due is only for testing purposes

--- contracts/mocks/tokens/WETH9Mocked.sol
+++ contracts/mocks/tokens/WETH9Mocked.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.4.22 <=0.6.8;
 
-import {WETH9} from '../../dependencies/weth/WETH9.sol';
+import {WETH9} from '../dependencies/weth/WETH9.sol';
 
 contract WETH9Mocked is WETH9 {
   // Mint not backed by Ether: only for testing purposes
