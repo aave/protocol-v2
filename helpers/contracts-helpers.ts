6aeabbe00b05149ab8e9b7d62e857d229e29bf10
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2020-11-06 12:12:40 -0300

    Add build params unit tests helpers

--- helpers/contracts-helpers.ts
+++ helpers/contracts-helpers.ts
@@ -1,4 +1,4 @@
-import {Contract, Signer, utils, ethers} from 'ethers';
+import {Contract, Signer, utils, ethers, BigNumberish} from 'ethers';
 import {signTypedData_v4} from 'eth-sig-util';
 import {fromRpcSig, ECDSASignature} from 'ethereumjs-util';
 import BigNumber from 'bignumber.js';
@@ -211,4 +211,56 @@ export const getSignatureFromTypedData = (
     data: typedData,
   });
   return fromRpcSig(signature);
+};
+
+export const buildLiquiditySwapParams = (
+  assetToSwapToList: tEthereumAddress[],
+  minAmountsToReceive: BigNumberish[],
+  swapAllBalances: BigNumberish[],
+  permitAmounts: BigNumberish[],
+  deadlines: BigNumberish[],
+  v: BigNumberish[],
+  r: (string | Buffer)[],
+  s: (string | Buffer)[]
+) => {
+  return ethers.utils.defaultAbiCoder.encode(
+    [
+      'address[]',
+      'uint256[]',
+      'bool[]',
+      'uint256[]',
+      'uint256[]',
+      'uint8[]',
+      'bytes32[]',
+      'bytes32[]',
+    ],
+    [assetToSwapToList, minAmountsToReceive, swapAllBalances, permitAmounts, deadlines, v, r, s]
+  );
+};
+
+export const buildRepayAdapterParams = (
+  assetToSwapToList: tEthereumAddress[],
+  leftoverAction: BigNumberish,
+  repayAmounts: BigNumberish[],
+  rateModes: BigNumberish[],
+  permitAmounts: BigNumberish[],
+  deadlines: BigNumberish[],
+  v: BigNumberish[],
+  r: (string | Buffer)[],
+  s: (string | Buffer)[]
+) => {
+  return ethers.utils.defaultAbiCoder.encode(
+    [
+      'address[]',
+      'uint256',
+      'uint256[]',
+      'uint256[]',
+      'uint256[]',
+      'uint256[]',
+      'uint8[]',
+      'bytes32[]',
+      'bytes32[]',
+    ],
+    [assetToSwapToList, leftoverAction, repayAmounts, rateModes, permitAmounts, deadlines, v, r, s]
+  );
 };
