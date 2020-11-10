57ed9efd58260c11f133c61c684a441b2a4d2aed
The3D <frangellaemilio@gmail.com>
2020-11-10 16:33:00 +0100

    Validated the user balance before isActive

--- contracts/libraries/logic/ValidationLogic.sol
+++ contracts/libraries/logic/ValidationLogic.sol
@@ -67,11 +67,11 @@ library ValidationLogic {
     address oracle
   ) external view {
 
-    (bool isActive,, , ) = reservesData[reserveAddress].configuration.getFlags();
-    require(isActive, Errors.VL_NO_ACTIVE_RESERVE);
-
     require(amount != 0, Errors.VL_INVALID_AMOUNT);
     require(amount <= userBalance, Errors.VL_NOT_ENOUGH_AVAILABLE_USER_BALANCE);
+
+    (bool isActive,, , ) = reservesData[reserveAddress].configuration.getFlags();
+    require(isActive, Errors.VL_NO_ACTIVE_RESERVE);
 
     require(
       GenericLogic.balanceDecreaseAllowed(
