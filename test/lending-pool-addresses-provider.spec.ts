c81047ca93bcd93e74ef768aa928e83d79ba87fb
The3D <frangellaemilio@gmail.com>
2020-11-10 14:57:09 +0100

    Fixes #123

--- test/lending-pool-addresses-provider.spec.ts
+++ test/lending-pool-addresses-provider.spec.ts
@@ -31,28 +31,61 @@ makeSuite('LendingPoolAddressesProvider', (testEnv: TestEnv) => {
     await expect(
       addressesProvider.setAddress(
         utils.keccak256(utils.toUtf8Bytes('RANDOM_ID')),
-        mockAddress,
-        ZERO_ADDRESS
+        mockAddress
       )
     ).to.be.revertedWith(INVALID_OWNER_REVERT_MSG);
+
+    await expect(
+      addressesProvider.setAddressAsProxy(
+        utils.keccak256(utils.toUtf8Bytes('RANDOM_ID')),
+        mockAddress
+      )
+    ).to.be.revertedWith(INVALID_OWNER_REVERT_MSG);
+ 
   });
 
-  it('Tests adding both a proxied and non-proxied addres with `setAddress()`', async () => {
+  it('Tests adding  a proxied address with `setAddressAsProxy()`', async () => {
     const {addressesProvider, users} = testEnv;
     const {INVALID_OWNER_REVERT_MSG} = ProtocolErrors;
 
     const currentAddressesProviderOwner = users[1];
 
-    const mockNonProxiedAddress = createRandomAddress();
-    const nonProxiedAddressId = utils.keccak256(utils.toUtf8Bytes('RANDOM_NON_PROXIED'));
 
     const mockLendingPool = await deployLendingPool();
     const proxiedAddressId = utils.keccak256(utils.toUtf8Bytes('RANDOM_PROXIED'));
 
+    const proxiedAddressSetReceipt = await waitForTx(
+      await addressesProvider
+        .connect(currentAddressesProviderOwner.signer)
+        .setAddressAsProxy(proxiedAddressId, mockLendingPool.address)
+    );
+
+    if (!proxiedAddressSetReceipt.events || proxiedAddressSetReceipt.events?.length < 1) {
+      throw new Error('INVALID_EVENT_EMMITED');
+    }
+
+    expect(proxiedAddressSetReceipt.events[0].event).to.be.equal('ProxyCreated');
+    expect(proxiedAddressSetReceipt.events[1].event).to.be.equal('AddressSet');
+    expect(proxiedAddressSetReceipt.events[1].args?.id).to.be.equal(proxiedAddressId);
+    expect(proxiedAddressSetReceipt.events[1].args?.newAddress).to.be.equal(
+      mockLendingPool.address
+    );
+    expect(proxiedAddressSetReceipt.events[1].args?.hasProxy).to.be.equal(true);
+  });
+
+
+  it('Tests adding a non proxied address with `setAddress()`', async () => {
+    const {addressesProvider, users} = testEnv;
+    const {INVALID_OWNER_REVERT_MSG} = ProtocolErrors;
+
+    const currentAddressesProviderOwner = users[1];
+    const mockNonProxiedAddress = createRandomAddress();
+    const nonProxiedAddressId = utils.keccak256(utils.toUtf8Bytes('RANDOM_NON_PROXIED'));
+
     const nonProxiedAddressSetReceipt = await waitForTx(
       await addressesProvider
         .connect(currentAddressesProviderOwner.signer)
-        .setAddress(nonProxiedAddressId, mockNonProxiedAddress, ZERO_ADDRESS)
+        .setAddress(nonProxiedAddressId, mockNonProxiedAddress)
     );
 
     expect(mockNonProxiedAddress.toLowerCase()).to.be.equal(
@@ -70,22 +103,5 @@ makeSuite('LendingPoolAddressesProvider', (testEnv: TestEnv) => {
     );
     expect(nonProxiedAddressSetReceipt.events[0].args?.hasProxy).to.be.equal(false);
 
-    const proxiedAddressSetReceipt = await waitForTx(
-      await addressesProvider
-        .connect(currentAddressesProviderOwner.signer)
-        .setAddress(proxiedAddressId, ZERO_ADDRESS, mockLendingPool.address)
-    );
-
-    if (!proxiedAddressSetReceipt.events || proxiedAddressSetReceipt.events?.length < 2) {
-      throw new Error('INVALID_EVENT_EMMITED');
-    }
-
-    expect(proxiedAddressSetReceipt.events[0].event).to.be.equal('ProxyCreated');
-    expect(proxiedAddressSetReceipt.events[1].event).to.be.equal('AddressSet');
-    expect(proxiedAddressSetReceipt.events[1].args?.id).to.be.equal(proxiedAddressId);
-    expect(proxiedAddressSetReceipt.events[1].args?.newAddress).to.be.equal(
-      mockLendingPool.address
-    );
-    expect(proxiedAddressSetReceipt.events[1].args?.hasProxy).to.be.equal(true);
   });
 });
