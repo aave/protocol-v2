6460dd9e034bf74b882f9f0e02a46ba1282a4d7e
The3D <frangellaemilio@gmail.com>
2020-11-10 17:21:59 +0100

    fixes #133

--- contracts/libraries/configuration/UserConfiguration.sol
+++ contracts/libraries/configuration/UserConfiguration.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.6.8;
 
+import {Errors} from '../helpers/Errors.sol';
 /**
  * @title UserConfiguration library
  * @author Aave
@@ -24,6 +25,7 @@ library UserConfiguration {
     uint256 reserveIndex,
     bool borrowing
   ) internal {
+    require(reserveIndex < 128, Errors.UL_INVALID_INDEX);
     self.data =
       (self.data & ~(1 << (reserveIndex * 2))) |
       (uint256(borrowing ? 1 : 0) << (reserveIndex * 2));
@@ -40,6 +42,7 @@ library UserConfiguration {
     uint256 reserveIndex,
     bool _usingAsCollateral
   ) internal {
+    require(reserveIndex < 128, Errors.UL_INVALID_INDEX);
     self.data =
       (self.data & ~(1 << (reserveIndex * 2 + 1))) |
       (uint256(_usingAsCollateral ? 1 : 0) << (reserveIndex * 2 + 1));
@@ -56,6 +59,7 @@ library UserConfiguration {
     pure
     returns (bool)
   {
+    require(reserveIndex < 128, Errors.UL_INVALID_INDEX);
     return (self.data >> (reserveIndex * 2)) & 3 != 0;
   }
 
@@ -70,6 +74,7 @@ library UserConfiguration {
     pure
     returns (bool)
   {
+    require(reserveIndex < 128, Errors.UL_INVALID_INDEX);
     return (self.data >> (reserveIndex * 2)) & 1 != 0;
   }
 
@@ -84,6 +89,7 @@ library UserConfiguration {
     pure
     returns (bool)
   {
+    require(reserveIndex < 128, Errors.UL_INVALID_INDEX);
     return (self.data >> (reserveIndex * 2 + 1)) & 1 != 0;
   }
 
