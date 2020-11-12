d2739e3a9c7970c5e1100b11c90e3ca804b9473e
David Racero <canillas.mail@gmail.com>
2020-11-12 14:43:14 +0100

    Add the Tenderly provider at set-bre task

--- tasks/misc/set-bre.ts
+++ tasks/misc/set-bre.ts
@@ -6,9 +6,12 @@ import {setDRE} from '../../helpers/misc-utils';
 task(`set-DRE`, `Inits the DRE, to have access to all the plugins' objects`).setAction(
   async (_, _DRE) => {
     setDRE(_DRE);
-    const provider = new _DRE.ethers.providers.Web3Provider(_DRE.tenderlyRPC as ExternalProvider);
-    //Set the ethers provider to the one we initialized so it targets the correct backend
-    _DRE.ethers.provider = provider;
+    const network = _DRE.network.name;
+    if (network.includes('tenderly')) {
+      const provider = new _DRE.ethers.providers.Web3Provider(_DRE.tenderlyRPC as any);
+      //Set the ethers provider to the one we initialized so it targets the correct backend
+      _DRE.ethers.provider = provider;
+    }
     return _DRE;
   }
 );
