66bb895cec00398286caf27f21a2d243d3d4173c
pistiner <59415933+orpistiner@users.noreply.github.com>
2020-11-24 14:53:09 +0200

    Updated running script of UserConfiguration library.

--- runUserConfigCLI.sh
+++ runUserConfigCLI.sh
@@ -1,1 +1,1 @@
-certoraRun specs/harness/UserConfigurationHarness.sol  --solc solc6.8 --verify UserConfigurationHarness:specs/UserConfiguration.spec --settings -useBitVectorTheory --staging master
+certoraRun specs/harness/UserConfigurationHarness.sol  --solc solc6.12 --verify UserConfigurationHarness:specs/UserConfiguration.spec --solc_args '--optimize' --settings -useBitVectorTheory --staging
