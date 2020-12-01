6ce019c04a496aa34c89c4b043a375fbc9e4f497
andyk <kyzia.ru@gmail.com>
2020-12-01 14:27:57 +0400

    add deployment task for UniswapRepayAdapter

--- tasks/deployments/deploy-UniswapLiquiditySwapAdapter.ts
+++ tasks/deployments/deploy-UniswapLiquiditySwapAdapter.ts
@@ -1,10 +1,10 @@
 import { task } from 'hardhat/config';
 
-import { UniswapRepayAdapterFactory } from '../../types';
+import { UniswapLiquiditySwapAdapterFactory } from '../../types';
 import { verifyContract } from '../../helpers/etherscan-verification';
 import { getFirstSigner } from '../../helpers/contracts-getters';
 
-const CONTRACT_NAME = 'UniswapRepayAdapter';
+const CONTRACT_NAME = 'UniswapLiquiditySwapAdapter';
 
 task(`deploy-${CONTRACT_NAME}`, `Deploys the ${CONTRACT_NAME} contract`)
   .addFlag('verify', `Verify ${CONTRACT_NAME} contract via Etherscan API.`)
@@ -20,15 +20,12 @@ task(`deploy-${CONTRACT_NAME}`, `Deploys the ${CONTRACT_NAME} contract`)
       '0x88757f2f99175387aB4C6a4b3067c77A695b0349', // lending  provider kovan address
       '0xfcd87315f0e4067070ade8682fcdbc3006631441', // uniswap router address
     ];
-    const uniswapRepayAdapter = await new UniswapRepayAdapterFactory(await getFirstSigner()).deploy(
-      args[0],
-      args[1]
-    );
+    const uniswapRepayAdapter = await new UniswapLiquiditySwapAdapterFactory(
+      await getFirstSigner()
+    ).deploy(args[0], args[1]);
     await uniswapRepayAdapter.deployTransaction.wait();
     console.log(`${CONTRACT_NAME}.address`, uniswapRepayAdapter.address);
     await verifyContract(uniswapRepayAdapter.address, args);
 
-    console.log(
-      `\tFinished ${CONTRACT_NAME}${CONTRACT_NAME}lDataProvider proxy and implementation deployment`
-    );
+    console.log(`\tFinished ${CONTRACT_NAME} proxy and implementation deployment`);
   });
