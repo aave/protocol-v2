695fc5c44292b3c5af326119b518a7d667b99806
nithin <nithin@fragments.org>
2020-12-14 10:05:52 -0500

    math update

--- contracts/protocol/tokenization/ampl/IncentivizedAAmplERC20.sol
+++ contracts/protocol/tokenization/ampl/IncentivizedAAmplERC20.sol
@@ -1,50 +1,24 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity 0.6.12;
 
-import {Context} from '../../dependencies/openzeppelin/contracts/Context.sol';
-import {IERC20} from '../../dependencies/openzeppelin/contracts/IERC20.sol';
-import {IERC20Detailed} from '../../dependencies/openzeppelin/contracts/IERC20Detailed.sol';
-import {SafeMath} from '../../dependencies/openzeppelin/contracts/SafeMath.sol';
-import {IAaveIncentivesController} from '../../interfaces/IAaveIncentivesController.sol';
+import {Context} from '../../../dependencies/openzeppelin/contracts/Context.sol';
+import {IERC20} from '../../../dependencies/openzeppelin/contracts/IERC20.sol';
+import {IERC20Detailed} from '../../../dependencies/openzeppelin/contracts/IERC20Detailed.sol';
+import {SafeMath} from '../../../dependencies/openzeppelin/contracts/SafeMath.sol';
+import {IAaveIncentivesController} from '../../../interfaces/IAaveIncentivesController.sol';
 
-/**
- * @title IncentivizedElasticAMPLMirrorERC20
- * @notice Basic implementation of a Mirrored Elastic ERC20 token.
- * @dev It behaves similar to Aave's IncentivizedERC20, but is elastic (ie balances change).
- *      Elasticity is mirrored based on the AMPL ERC-20 token contract.
- *      The `ElasticAMPLMirrorERC20` balances expand and contract proportionally based on
- *      how the referenced AMPL token expands and contracts.
- *
- *      The AMPL token exhibits elastic balances by representing public balances using 2
- *      numbers. A hidden unit called gons and a scalar value called `gonsPerAMPL`.
- *      Public ampl balances are represented as the product of the hidden balance and the scalar.
- *        => public_balance = gon_balance * gonsPerAMPL
- *      Thus by changing the scalar up/down, all token balances are changed proportionally.
- *
- *      The `ElasticAMPLMirrorERC20` also uses 2 numbers to represent public balances similar to AMPL,
- *      a hidden 'internal balance' and a scalar multiple (`gonsPerAMPL`).
- *        => public_balance = internal_balance * gonsPerAMPL
- *      It 'mimics' the AMPL's elasticity by mirroring the scalar multiple value used by the AMPL token.
- *      Thus when AMPL balances expand or contract, `ElasticAMPLMirrorERC20` balances also change by the same proportion.
- *
- * @author AmpleforthEng & Aave
- **/
-contract IncentivizedElasticAMPLMirrorERC20 is Context, IERC20, IERC20Detailed {
+// TODO: optimize external calls for AMPL scaling factor and reads
+contract IncentivizedAAmplERC20 is Context, IERC20, IERC20Detailed {
   using SafeMath for uint256;
 
   IAaveIncentivesController internal immutable _incentivesController;
 
-  address public immutable AMPL_ADDRESS;
-
-  uint256 private constant MAX_UINT256 = ~uint256(0); // (2^256) - 1
-  uint256 private constant AMPL_DECIMALS = 9;
-  uint256 private constant INITIAL_AMPL_SUPPLY = 50 * 10**6 * 10**AMPL_DECIMALS;
-  uint256 private constant TOTAL_GONS = MAX_UINT256 - (MAX_UINT256 % INITIAL_AMPL_SUPPLY);
-
-  mapping(address => uint256) internal _internalBalances;
+  mapping(address => uint256) private _balances;
   mapping(address => mapping(address => uint256)) private _allowances;
 
-  uint256 internal _totalSupplyInternal;
+  uint256 internal _totalScaledAMPLDeposited;
+  uint256 internal _totalSupply;
+
   string private _name;
   string private _symbol;
   uint8 private _decimals;
@@ -60,7 +34,6 @@ contract IncentivizedElasticAMPLMirrorERC20 is Context, IERC20, IERC20Detailed {
     _symbol = symbol;
     _decimals = decimals;
     _incentivesController = IAaveIncentivesController(incentivesController);
-    AMPL_ADDRESS = baseAsset;
   }
 
   /**
@@ -84,25 +57,28 @@ contract IncentivizedElasticAMPLMirrorERC20 is Context, IERC20, IERC20Detailed {
     return _decimals;
   }
 
+
   /**
    * @return The total supply of the token
+   *
+   *  totalSupply() = unborrowed + borrowed
+   *                = (totalScaledAMPLDeposited - totalScaledAMPLBorrowed)/AMPL_SCALAR + totalAMPLBorrowed
    **/
   function totalSupply() public view virtual override returns (uint256) {
-    return _totalSupplyInternal.div(gonsPerAMPL());
+    uint256 totalAMPLBorrowed;
+    uint256 totalScaledAMPLBorrowed;
+    (totalAMPLBorrowed, totalScaledAMPLBorrowed) = getAMPLBorrowData();
+    return _totalScaledAMPLDeposited.sub(totalScaledAMPLBorrowed).div(getAMPLScalar()).add(totalAMPLBorrowed);
   }
 
   /**
    * @return The external balance of the token
+   *
+   *  balanceOf(account) = account's share of (unborrowed + borrowed)
+   *                     = _balances[account]/_totalSupply * totalSupply()
    **/
   function balanceOf(address account) public view virtual override returns (uint256) {
-    return _internalBalances[account].div(gonsPerAMPL());
-  }
-
-  /**
-   * @return The scalar balance multiple
-   **/
-  function gonsPerAMPL() private view returns (uint256) {
-    return TOTAL_GONS.div(IERC20(AMPL_ADDRESS).totalSupply());
+    return _balances[account].mul(totalSupply()).div(_totalSupply);
   }
 
   /**
@@ -159,7 +135,7 @@ contract IncentivizedElasticAMPLMirrorERC20 is Context, IERC20, IERC20Detailed {
     _approve(
       sender,
       _msgSender(),
-      _allowances[sender][_msgSender()].sub(amount, 'ElasticAMPLMirrorERC20: transfer amount exceeds allowance')
+      _allowances[sender][_msgSender()].sub(amount, 'AAmplERC20: transfer amount exceeds allowance')
     );
     emit Transfer(sender, recipient, amount);
     return true;
@@ -192,34 +168,40 @@ contract IncentivizedElasticAMPLMirrorERC20 is Context, IERC20, IERC20Detailed {
       spender,
       _allowances[_msgSender()][spender].sub(
         subtractedValue,
-        'ElasticAMPLMirrorERC20: decreased allowance below zero'
+        'AAmplERC20: decreased allowance below zero'
       )
     );
     return true;
   }
 
+  /*
+    wkt, amount = _transferAmount/_totalSupply * totalSupply()
+
+    internal transfer amount for book-keeping is calculated as:
+    _transferAmount = (amount * _totalSupply) / totalSupply()
+
+    _balances[sender] -= _transferAmount
+    _balances[recipient] += _transferAmount
+  */
   function _transfer(
     address sender,
     address recipient,
     uint256 amount
   ) internal virtual {
-    require(sender != address(0), 'ElasticAMPLMirrorERC20: transfer from the zero address');
-    require(recipient != address(0), 'ElasticAMPLMirrorERC20: transfer to the zero address');
+    require(sender != address(0), 'AAmplERC20: transfer from the zero address');
+    require(recipient != address(0), 'AAmplERC20: transfer to the zero address');
 
     _beforeTokenTransfer(sender, recipient, amount);
 
-    uint256 gonsPerAMPL_ = gonsPerAMPL();
-    uint256 internalAmount = amount.mul(gonsPerAMPL_);
+    uint256 currentTotalSupply = totalSupply();
+    uint256 oldSenderBalance = balanceOf(sender);
+    uint256 oldRecipientBalance = balanceOf(recipient);
 
-    uint256 oldSenderBalanceInternal = _internalBalances[sender];
-    _internalBalances[sender] = oldSenderBalanceInternal.sub(internalAmount, 'ElasticAMPLMirrorERC20: transfer amount exceeds balance');
-    uint256 oldRecipientBalanceInternal = _internalBalances[recipient];
-    _internalBalances[recipient] = _internalBalances[recipient].add(internalAmount);
+    uint256 _transferAmount = amount.mul(_totalSupply).div(currentTotalSupply);
+    _balances[sender] = _balances[sender].sub(_transferAmount, 'AAmplERC20: transfer amount exceeds balance');
+    _balances[recipient] = _balances[recipient].add(_transferAmount);
 
     if(address(_incentivesController) != address(0)) {
-      uint256 currentTotalSupply = _totalSupplyInternal.div(gonsPerAMPL_);
-      uint256 oldSenderBalance = oldSenderBalanceInternal.div(gonsPerAMPL_);
-      uint256 oldRecipientBalance = oldRecipientBalanceInternal.div(gonsPerAMPL_);
       _incentivesController.handleAction(sender, currentTotalSupply, oldSenderBalance);
       if (sender != recipient) {
         _incentivesController.handleAction(recipient, currentTotalSupply, oldRecipientBalance);
@@ -227,44 +209,87 @@ contract IncentivizedElasticAMPLMirrorERC20 is Context, IERC20, IERC20Detailed {
     }
   }
 
+
+  /*
+    Before 'amount' is mint to account wkt,
+
+    _balances[account] / _totalSupply = balanceOf(account) / totalSupply()
+
+    We calculate _mintAmount such that the following holds true
+
+    (_balances[account] + _mintAmount) / (_totalSupply + _mintAmount)
+      = (balanceOf(account) + amount)/ (totalSupply() + amount)
+
+    newTotalSupply = totalSupply() + amount
+    newAccountBalance = balanceOf(account) + amount
+    remainingAccountBalance = totalSupply() - balanceOf(account)
+
+    Thus:
+    => _mintAmount = (_totalSupply * newAccountBalance) - (_balances[account] * newTotalSupply) / remainingAccountBalance
+  */
   function _mint(address account, uint256 amount) internal virtual {
-    require(account != address(0), 'ElasticAMPLMirrorERC20: mint to the zero address');
+    require(account != address(0), 'AAmplERC20: mint to the zero address');
 
     _beforeTokenTransfer(address(0), account, amount);
 
-    uint256 gonsPerAMPL_ = gonsPerAMPL();
-    uint256 internalAmount = amount.mul(gonsPerAMPL_);
+    uint256 oldTotalSupply = totalSupply();
+    uint256 oldAccountBalance = balanceOf(account);
+    uint256 oldRemainingAccountBalance = oldTotalSupply.sub(oldAccountBalance);
 
-    uint256 oldTotalSupplyInternal = _totalSupplyInternal;
-    _totalSupplyInternal = oldTotalSupplyInternal.add(internalAmount);
+    uint256 _oldAccountBalance = _balances[account];
+    uint256 _oldTotalSupply = _totalSupply;
 
-    uint256 oldAccountBalanceInternal = _internalBalances[account];
-    _internalBalances[account] = oldAccountBalanceInternal.add(internalAmount);
+    uint256 newTotalSupply = oldTotalSupply.add(amount);
+    uint256 newAccountBalance = oldAccountBalance.add(amount);
+
+    uint256 _mintAmount = _oldTotalSupply
+      .mul(newAccountBalance)
+      .sub(_oldAccountBalance.mul(newTotalSupply))
+      .div(oldRemainingAccountBalance);
+
+    _totalSupply = _totalSupply.add(_mintAmount);
+    _balances[account] = _oldAccountBalance.add(_mintAmount);
+
+    // NOTE: this additional book keeping to keep track of 'unborrowed' AMPLs
+    _totalScaledAMPLDeposited = _totalScaledAMPLDeposited.add(amount.mul(getAMPLScalar()));
 
     if (address(_incentivesController) != address(0)) {
-      uint256 oldTotalSupply = oldTotalSupplyInternal.div(gonsPerAMPL_);
-      uint256 oldAccountBalance = oldAccountBalanceInternal.div(gonsPerAMPL_);
       _incentivesController.handleAction(account, oldTotalSupply, oldAccountBalance);
     }
   }
 
+
+  /*
+    Calculated same as above:
+    => _burnAmount = (_balances[account] * newTotalSupply) - (_totalSupply * newAccountBalance) / remainingAccountBalance
+  */
   function _burn(address account, uint256 amount) internal virtual {
-    require(account != address(0), 'ElasticAMPLMirrorERC20: burn from the zero address');
+    require(account != address(0), 'AAmplERC20: burn from the zero address');
 
     _beforeTokenTransfer(account, address(0), amount);
 
-    uint256 gonsPerAMPL_ = gonsPerAMPL();
-    uint256 internalAmount = amount.mul(gonsPerAMPL_);
+    uint256 oldTotalSupply = totalSupply();
+    uint256 oldAccountBalance = balanceOf(account);
+    uint256 oldRemainingAccountBalance = oldTotalSupply.sub(oldAccountBalance);
 
-    uint256 oldTotalSupplyInternal = _totalSupplyInternal;
-    _totalSupplyInternal = oldTotalSupplyInternal.sub(internalAmount);
+    uint256 _oldAccountBalance = _balances[account];
+    uint256 _oldTotalSupply = _totalSupply;
 
-    uint256 oldAccountBalanceInternal = _internalBalances[account];
-    _internalBalances[account] = oldAccountBalanceInternal.sub(internalAmount, 'ElasticAMPLMirrorERC20: burn amount exceeds balance');
+    uint256 newTotalSupply = oldTotalSupply.sub(amount);
+    uint256 newAccountBalance = oldAccountBalance.sub(amount);
+
+    uint256 burnAmount = _oldAccountBalance
+      .mul(newTotalSupply)
+      .sub(_oldTotalSupply.mul(newAccountBalance))
+      .div(oldRemainingAccountBalance);
+
+    _totalSupply = _totalSupply.sub(burnAmount);
+    _balances[account] = _oldAccountBalance.sub(burnAmount, 'AAmplERC20: burn amount exceeds balance');
+
+    // NOTE: this additional book keeping to keep track of 'unborrowed' AMPLs
+    _totalScaledAMPLDeposited = _totalScaledAMPLDeposited.sub(amount.mul(getAMPLScalar()));
 
     if (address(_incentivesController) != address(0)) {
-      uint256 oldTotalSupply = oldTotalSupplyInternal.div(gonsPerAMPL_);
-      uint256 oldAccountBalance = oldAccountBalanceInternal.div(gonsPerAMPL_);
       _incentivesController.handleAction(account, oldTotalSupply, oldAccountBalance);
     }
   }
@@ -274,8 +299,8 @@ contract IncentivizedElasticAMPLMirrorERC20 is Context, IERC20, IERC20Detailed {
     address spender,
     uint256 amount
   ) internal virtual {
-    require(owner != address(0), 'ElasticAMPLMirrorERC20: approve from the zero address');
-    require(spender != address(0), 'ElasticAMPLMirrorERC20: approve to the zero address');
+    require(owner != address(0), 'AAmplERC20: approve from the zero address');
+    require(spender != address(0), 'AAmplERC20: approve to the zero address');
 
     _allowances[owner][spender] = amount;
     emit Approval(owner, spender, amount);
@@ -298,4 +323,10 @@ contract IncentivizedElasticAMPLMirrorERC20 is Context, IERC20, IERC20Detailed {
     address to,
     uint256 amount
   ) internal virtual {}
+
+  /**
+   * @return The scalar balance multiple
+   **/
+  function getAMPLBorrowData() internal virtual view returns (uint256, uint256) { }
+  function getAMPLScalar() internal virtual view returns (uint256) { }
 }
