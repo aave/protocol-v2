55f4017ddc4b1f378ba26ab9e697b7ca6e39114f
Kirill <krboktv@gmail.com>
2021-02-16 16:54:45 +0300

    WIP: Add CHAINLINK feeds

--- markets/aave/commons.ts
+++ markets/aave/commons.ts
@@ -216,6 +216,7 @@ export const CommonsConfig: ICommonConfiguration = {
       YFI: '0xC5d1B1DEb2992738C0273408ac43e1e906086B6C',
       ZRX: '0xBc3f28Ccc21E9b5856E81E6372aFf57307E2E883',
       USD: '0x9326BFA02ADD2366b30bacB125260Af641031331',
+      '1INCH': ZERO_ADDRESS,
     },
     [EthereumNetwork.ropsten]: {
       AAVE: ZERO_ADDRESS,
@@ -238,6 +239,7 @@ export const CommonsConfig: ICommonConfiguration = {
       YFI: ZERO_ADDRESS,
       ZRX: '0x1d0052e4ae5b4ae4563cbac50edc3627ca0460d7',
       USD: '0x8468b2bDCE073A157E560AA4D9CcF6dB1DB98507',
+      '1INCH': ZERO_ADDRESS,
     },
     [EthereumNetwork.main]: {
       AAVE: '0x6Df09E975c830ECae5bd4eD9d90f3A95a4f88012',
@@ -260,6 +262,7 @@ export const CommonsConfig: ICommonConfiguration = {
       YFI: '0x7c5d4F8345e66f68099581Db340cd65B078C41f4',
       ZRX: '0x2Da4983a622a8498bb1a21FaE9D8F6C664939962',
       USD: '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419',
+      '1INCH': '0x72AFAECF99C9d9C8215fF44C77B94B99C28741e8',
     },
     [EthereumNetwork.tenderlyMain]: {
       AAVE: '0x6Df09E975c830ECae5bd4eD9d90f3A95a4f88012',
@@ -282,6 +285,7 @@ export const CommonsConfig: ICommonConfiguration = {
       YFI: '0x7c5d4F8345e66f68099581Db340cd65B078C41f4',
       ZRX: '0x2Da4983a622a8498bb1a21FaE9D8F6C664939962',
       USD: '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419',
+      '1INCH': '0x72AFAECF99C9d9C8215fF44C77B94B99C28741e8',
     },
   },
   ReserveAssets: {
