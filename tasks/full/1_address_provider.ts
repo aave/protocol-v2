5ca1a4f05b23c297c3e8241e3c6960f8d184de96
Zer0dot <zer0dot.dev@gmail.com>
2021-02-18 16:49:29 -0500

    Re-enabled Sec 3

--- tasks/full/1_address_provider.ts
+++ tasks/full/1_address_provider.ts
@@ -82,15 +82,14 @@ task(
     // 2. Deploy address provider and set genesis manager
     const addressesProvider = await deployLendingPoolAddressesProvider(MarketId, verify);
     
-    // TEMPORARILY DISABLING SEC. 3 FOR GOVERNANCE USE
-
+    // DISABLE SEC. 3 FOR GOVERNANCE USE!
     // 3. Set the provider at the Registry
-    // await waitForTx(
-    //   await addressesProviderRegistry.registerAddressesProvider(
-    //     addressesProvider.address,
-    //     ProviderId
-    //   )
-    // );
+    await waitForTx(
+      await addressesProviderRegistry.registerAddressesProvider(
+        addressesProvider.address,
+        ProviderId
+      )
+    );
 
     // 4. Set pool admins
     
