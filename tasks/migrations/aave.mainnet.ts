f8d8f402324fb150dc21b1c3328f67b0149e5452
Zer0dot <zer0dot.dev@gmail.com>
2021-02-22 09:57:48 -0500

    Slight fixed for fork deployment

--- tasks/migrations/aave.mainnet.ts
+++ tasks/migrations/aave.mainnet.ts
@@ -29,7 +29,7 @@ task('aave:mainnet', 'Deploy development enviroment')
     console.log('4. Deploy Data Provider');
     await DRE.run('full:data-provider', { pool: POOL_NAME });
 
-    console.log('5. Deploy weth Gethway provider');
+    console.log('5. Deploy WETH Gateway');
     await DRE.run('full-deploy-weth-gateway', { pool: POOL_NAME });
 
     console.log('6. Initialize lending pool');
