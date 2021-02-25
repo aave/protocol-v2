b0e14ee12aa1c4e4311f30c09ab66b71302d4860
Nithin Krishna <nithin@fragments.org>
2021-02-25 15:27:32 -0500

    Update contracts/protocol/tokenization/ampl/AAmplToken.sol

--- contracts/protocol/tokenization/ampl/AAmplToken.sol
+++ contracts/protocol/tokenization/ampl/AAmplToken.sol
@@ -587,7 +587,7 @@ contract AAmplToken is VersionedInitializable, IncentivizedERC20, IAToken {
 
   /**
    * @dev balanceOfScaled = balanceInternal / totalSupplyInternal * totalSupplyScaled
-   *                       = balanceInternal . λ
+   *                       = λ . balanceInternal
    **/
   function _balanceOfScaled(uint256 balanceInternal, uint256 totalSupplyInternal, uint256 totalSupplyScaled) private pure returns (uint256) {
     return balanceInternal.mul(totalSupplyScaled).div(totalSupplyInternal);
