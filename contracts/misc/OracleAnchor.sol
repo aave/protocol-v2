f29afb1b475736e017d72cfbaf3a365449a898ae
Daniel Martín Jiménez <dani.elnota@gmail.com>
2021-03-03 16:26:01 +0100

    fix logic

--- contracts/misc/OracleAnchor.sol
+++ contracts/misc/OracleAnchor.sol
@@ -1,20 +1,12 @@
 pragma solidity 0.6.12;
 
-import {Ownable} from '../dependencies/openzeppelin/contracts/Ownable.sol';
-
-contract AaveOracle is Ownable {
+contract AaveOracle  {
   event AssetSourceUpdated(address indexed asset, address indexed source);
   event OracleSystemMigrated();
 
-  constructor() public {
-    emit OracleSystemMigrated();
-  }
-
-  function setAssetSources(address[] calldata assets, address[] calldata sources)
-    external
-    onlyOwner
-  {
+  constructor(address[] calldata assets, address[] calldata sources) public {
     require(assets.length == sources.length, 'INCONSISTENT_PARAMS_LENGTH');
+    emit OracleSystemMigrated();
     for (uint256 i = 0; i < assets.length; i++) {
       emit AssetSourceUpdated(assets[i], sources[i]);
     }
