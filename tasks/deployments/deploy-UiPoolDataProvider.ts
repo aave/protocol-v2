516cbc7349065bcea1f9d9e68810b12428ed3b36
sendra <polsendra@gmail.com>
2021-03-30 13:17:08 +0200

    updated deployment scripts

--- tasks/deployments/deploy-UiPoolDataProvider.ts
+++ tasks/deployments/deploy-UiPoolDataProvider.ts
@@ -1,8 +1,6 @@
 import { task } from 'hardhat/config';
-
-import { UiPoolDataProviderFactory } from '../../types';
-import { verifyContract } from '../../helpers/etherscan-verification';
-import { eContractid } from '../../helpers/types';
+import { eContractid, eEthereumNetwork, ePolygonNetwork } from '../../helpers/types';
+import { deployUiPoolDataProvider } from '../../helpers/contracts-deployments';
 
 task(`deploy-${eContractid.UiPoolDataProvider}`, `Deploys the UiPoolDataProvider contract`)
   .addFlag('verify', 'Verify UiPoolDataProvider contract via Etherscan API.')
@@ -13,15 +11,31 @@ task(`deploy-${eContractid.UiPoolDataProvider}`, `Deploys the UiPoolDataProvider
       throw new Error('INVALID_CHAIN_ID');
     }
 
+    const addressesByNetwork = {
+      // [eEthereumNetwork.kovan]: {
+      //   incentivesController: '',
+      //   aaveOracle: '',
+      // },
+      // [eEthereumNetwork.main]: {
+      //   incentivesController: '',
+      //   aaveOracle: '0xa50ba011c48153de246e5192c8f9258a2ba79ca9',
+      // },
+      [ePolygonNetwork.matic]: {
+        incentivesController: '0x357D51124f59836DeD84c8a1730D72B749d8BC23',
+        aaveOracle: '0x21451bD7b528896B4AB2b9764b521D6ed641708d',
+      },
+    };
+
     console.log(`\n- UiPoolDataProvider deployment`);
 
     console.log(`\tDeploying UiPoolDataProvider implementation ...`);
-    const uiPoolDataProvider = await new UiPoolDataProviderFactory(
-      await localBRE.ethers.provider.getSigner()
-    ).deploy();
-    await uiPoolDataProvider.deployTransaction.wait();
-    console.log('uiPoolDataProvider.address', uiPoolDataProvider.address);
-    await verifyContract(uiPoolDataProvider.address, []);
+    const UiPoolDataProvider = await deployUiPoolDataProvider(
+      [
+        addressesByNetwork[localBRE.network.name].incentivesController,
+        addressesByNetwork[localBRE.network.name].aaveOracle,
+      ],
+      verify
+    );
 
-    console.log(`\tFinished UiPoolDataProvider proxy and implementation deployment`);
+    console.log(`\tFinished UiPoolDataProvider deployment: ${UiPoolDataProvider.address}`);
   });
