2ae26808345ab7878ae98ddca4cf29b7d82e95dd
Hadrien Charlanes <hadrien@variabl.io>
2021-04-01 14:27:09 +0200

    Added permit to IAToken interface

--- contracts/interfaces/IAToken.sol
+++ contracts/interfaces/IAToken.sol
@@ -96,6 +96,27 @@ interface IAToken is IERC20, IScaledBalanceToken, IInitializableAToken {
   function handleRepayment(address user, uint256 amount) external;
 
   /**
+   * @dev implements the permit function as for
+   * https://github.com/ethereum/EIPs/blob/8a34d644aacf0f9f8f00815307fd7dd5da07655f/EIPS/eip-2612.md
+   * @param owner The owner of the funds
+   * @param spender The spender
+   * @param value The amount
+   * @param deadline The deadline timestamp, type(uint256).max for max deadline
+   * @param v Signature param
+   * @param s Signature param
+   * @param r Signature param
+   */
+  function permit(
+    address owner,
+    address spender,
+    uint256 value,
+    uint256 deadline,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  ) external;
+
+  /**
    * @dev Returns the address of the incentives controller contract
    **/
   function getIncentivesController() external view returns (IAaveIncentivesController);
