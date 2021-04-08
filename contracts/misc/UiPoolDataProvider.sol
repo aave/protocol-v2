e81e5e61fd40c755b3b90422634d350e97f28143
sendra <polsendra@gmail.com>
2021-04-08 13:51:59 +0200

    added mock user unclaimed rewards

--- contracts/misc/UiPoolDataProvider.sol
+++ contracts/misc/UiPoolDataProvider.sol
@@ -58,6 +58,7 @@ contract UiPoolDataProvider is IUiPoolDataProvider {
     returns (
       AggregatedReserveData[] memory,
       UserReserveData[] memory,
+      uint256,
       uint256
     )
   {
@@ -163,6 +164,6 @@ contract UiPoolDataProvider is IUiPoolDataProvider {
       }
     }
 
-    return (reservesData, userReservesData, oracle.getAssetPrice(MOCK_USD_ADDRESS));
+    return (reservesData, userReservesData, oracle.getAssetPrice(MOCK_USD_ADDRESS), 0);
   }
 }
