a5ab0a6e3b3b4715ff692740ae9b68fe2d407ab5
sendra <polsendra@gmail.com>
2021-04-09 10:57:01 +0200

    added mumbai addresses

--- tasks/deployments/deploy-UiPoolDataProvider.ts
+++ tasks/deployments/deploy-UiPoolDataProvider.ts
@@ -6,7 +6,7 @@ task(`deploy-${eContractid.UiPoolDataProvider}`, `Deploys the UiPoolDataProvider
   .addFlag('verify', 'Verify UiPoolDataProvider contract via Etherscan API.')
   .setAction(async ({ verify }, localBRE) => {
     await localBRE.run('set-DRE');
-
+    console.log('founds: ', await localBRE.ethers.getSigners());
     if (!localBRE.network.config.chainId) {
       throw new Error('INVALID_CHAIN_ID');
     }
@@ -23,6 +23,10 @@ task(`deploy-${eContractid.UiPoolDataProvider}`, `Deploys the UiPoolDataProvider
       [ePolygonNetwork.matic]: {
         incentivesController: '0x357D51124f59836DeD84c8a1730D72B749d8BC23',
         aaveOracle: '0x21451bD7b528896B4AB2b9764b521D6ed641708d',
+      },
+      [ePolygonNetwork.mumbai]: {
+        incentivesController: '0xc31c45a46e55f714f9CB2b43Ae688487C16616e2',
+        aaveOracle: '0x584c84AA7aE807e18957f8E3693BccBD482357E2',
       },
     };
 
