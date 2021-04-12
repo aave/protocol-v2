47b3d8ff65c2280a183edee8b626123152090676
sendra <polsendra@gmail.com>
2021-04-12 10:56:33 +0200

    new mumbai addresses

--- tasks/deployments/deploy-UiPoolDataProvider.ts
+++ tasks/deployments/deploy-UiPoolDataProvider.ts
@@ -24,8 +24,8 @@ task(`deploy-${eContractid.UiPoolDataProvider}`, `Deploys the UiPoolDataProvider
         aaveOracle: '0x21451bD7b528896B4AB2b9764b521D6ed641708d',
       },
       [ePolygonNetwork.mumbai]: {
-        incentivesController: '0xCA8f76C244271A857CE76263e60d294EB24186A8',
-        aaveOracle: '0x3cC6db8dBBa233Fe342c2c8379993aCa231674bd',
+        incentivesController: '0xd3aC1EBa90c21D70464dDafd30E3Ff8941002929',
+        aaveOracle: '0x48beE000e8f86d0ED8c0fB22D63e789b0cE9Aed1',
       },
     };
 
