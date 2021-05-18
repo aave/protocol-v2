34c9bf209e70b5a2a33f1be3467013032918a9f3
miguelmtzinf <miguelmartinezariasinf@gmail.com>
2021-05-18 10:40:22 +0200

    fix: Fix typo of tenderlyRPC for  tenderlyNetwork

--- tasks/migrations/aave.mainnet.ts
+++ tasks/migrations/aave.mainnet.ts
@@ -46,8 +46,8 @@ task('aave:mainnet', 'Deploy development enviroment')
     }
 
     if (usingTenderly()) {
-      const postDeployHead = DRE.tenderlyRPC.getHead();
-      const postDeployFork = DRE.tenderlyRPC.getFork();
+      const postDeployHead = DRE.tenderlyNetwork.getHead();
+      const postDeployFork = DRE.tenderlyNetwork.getFork();
       console.log('Tenderly Info');
       console.log('- Head', postDeployHead);
       console.log('- Fork', postDeployFork);
