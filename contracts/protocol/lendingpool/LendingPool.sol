09233d09bf06b1f7b5662bca8d1714d28c48a568
Hadrien Charlanes <hadrien@variabl.io>
2021-06-21 12:25:33 +0200

    feat: added storage and check that repayer is not the last borrower of a reserve

--- contracts/protocol/lendingpool/LendingPool.sol
+++ contracts/protocol/lendingpool/LendingPool.sol
@@ -240,7 +240,6 @@ contract LendingPool is VersionedInitializable, ILendingPool, LendingPoolStorage
 
   ///@inheritdoc ILendingPool
   function rebalanceStableBorrowRate(address asset, address user) external override whenNotPaused {
-    
     DataTypes.ReserveData storage reserve = _reserves[asset];
     DataTypes.ReserveCache memory reserveCache = reserve.cache();
 
@@ -793,6 +792,9 @@ contract LendingPool is VersionedInitializable, ILendingPool, LendingPoolStorage
       vars.releaseUnderlying ? vars.amount : 0
     );
 
+    _lastBorrower = vars.user;
+    _lastBorrowTimestamp = uint40(block.timestamp);
+
     if (vars.releaseUnderlying) {
       IAToken(reserveCache.aTokenAddress).transferUnderlyingTo(vars.user, vars.amount);
     }
@@ -908,6 +910,8 @@ contract LendingPool is VersionedInitializable, ILendingPool, LendingPoolStorage
     DataTypes.InterestRateMode interestRateMode = DataTypes.InterestRateMode(rateMode);
 
     ValidationLogic.validateRepay(
+      _lastBorrower,
+      _lastBorrowTimestamp,
       reserveCache,
       amount,
       interestRateMode,
