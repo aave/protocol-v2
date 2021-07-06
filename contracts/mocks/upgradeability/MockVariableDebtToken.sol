91709e82e206f6c1edccab3466e320db05f69abe
Hadrien Charlanes <hadrien@variabl.io>
2021-07-06 11:54:13 +0200

    fix: updated mocked debt contracts for upgradability tests

--- contracts/mocks/upgradeability/MockVariableDebtToken.sol
+++ contracts/mocks/upgradeability/MockVariableDebtToken.sol
@@ -5,6 +5,6 @@ import {VariableDebtToken} from '../../protocol/tokenization/VariableDebtToken.s
 
 contract MockVariableDebtToken is VariableDebtToken {
   function getRevision() internal pure override returns (uint256) {
-    return 0x2;
+    return 0x3;
   }
 }
