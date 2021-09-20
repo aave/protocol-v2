a7337104b9440eaaca34f4d666e1673105976502
karotjal <canillas.mail@gmail.com>
2021-09-20 12:21:12 +0200

    config: adjusted risk parameters for avalanche

--- markets/avalanche/reservesConfigs.ts
+++ markets/avalanche/reservesConfigs.ts
@@ -35,31 +35,31 @@ export const strategyWBTC: IReserveParams = {
   stableBorrowRateEnabled: false,
   reserveDecimals: '8',
   aTokenImpl: eContractid.AToken,
-  reserveFactor: '0',
+  reserveFactor: '2000',
 };
 
 export const strategyAAVE: IReserveParams = {
   strategy: rateStrategyVolatileOne,
   baseLTVAsCollateral: '5000',
   liquidationThreshold: '6500',
-  liquidationBonus: '11000',
+  liquidationBonus: '10800',
   borrowingEnabled: false,
   stableBorrowRateEnabled: false,
   reserveDecimals: '18',
   aTokenImpl: eContractid.AToken,
-  reserveFactor: '0',
+  reserveFactor: '2000',
 };
 
 export const strategyDAI: IReserveParams = {
   strategy: rateStrategyStableOne,
-  baseLTVAsCollateral: '0',
-  liquidationThreshold: '0',
-  liquidationBonus: '0',
+  baseLTVAsCollateral: '7500',
+  liquidationThreshold: '8000',
+  liquidationBonus: '10500',
   borrowingEnabled: true,
   stableBorrowRateEnabled: false,
   reserveDecimals: '18',
   aTokenImpl: eContractid.AToken,
-  reserveFactor: '0',
+  reserveFactor: '1000',
 };
 
 export const strategyUSDT: IReserveParams = {
@@ -71,17 +71,17 @@ export const strategyUSDT: IReserveParams = {
   stableBorrowRateEnabled: false,
   reserveDecimals: '6',
   aTokenImpl: eContractid.AToken,
-  reserveFactor: '0',
+  reserveFactor: '1000',
 };
 
 export const strategyUSDC: IReserveParams = {
   strategy: rateStrategyStableOne,
-  baseLTVAsCollateral: '0',
-  liquidationThreshold: '0',
-  liquidationBonus: '0',
+  baseLTVAsCollateral: '7500',
+  liquidationThreshold: '8000',
+  liquidationBonus: '10500',
   borrowingEnabled: true,
   stableBorrowRateEnabled: false,
   reserveDecimals: '6',
   aTokenImpl: eContractid.AToken,
-  reserveFactor: '0',
+  reserveFactor: '1000',
 };
