c6fc1500d21e6032d1b9bf8ed121bc39142304ff
Patrick <pakim249@ischool.berkeley.edu>
2021-11-22 23:09:49 -0800

    Updating hardhat-etherscan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -22,7 +22,7 @@ require('dotenv').config();
 
 import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-waffle';
-import 'temp-hardhat-etherscan';
+import '@nomiclabs/hardhat-etherscan';
 import 'hardhat-gas-reporter';
 import 'hardhat-typechain';
 import '@tenderly/hardhat-tenderly';
