75d75e453501e6f8f4c154d97575f81e679bd04a
kartojal <canillas.mail@gmail.com>
2022-08-09 15:01:59 +0200

    feat: add goerli constants for ui helpers

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -86,6 +86,7 @@ export const chainlinkAggregatorProxy = {
   'arbitrum-rinkeby': '0x5f0423B1a6935dc5596e7A24d98532b67A0AeFd8',
   arbitrum: '0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612',
   rinkeby: '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e',
+  goerli: '0x9F54B624fb17d07816C5552f8AB133c21b0322cD',
 };
 
 export const chainlinkEthUsdAggregatorProxy = {
@@ -99,4 +100,5 @@ export const chainlinkEthUsdAggregatorProxy = {
   'arbitrum-rinkeby': '0x5f0423B1a6935dc5596e7A24d98532b67A0AeFd8',
   arbitrum: '0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612',
   rinkeby: '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e',
+  goerli: '0x9F54B624fb17d07816C5552f8AB133c21b0322cD',
 };
